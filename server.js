b35044c518a2bb307ea1ce23696d8f2641b3b4b1
Philip Ng <phil__n@hotmail.com>
2020-04-05 17:00:57 -0400

    changed link to database on heroku and deleted unnecessary lines of code not needed with mongoose 5

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,19 +17,25 @@ app.use(express.static("public"));
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(
-  // process.env.MONGODB_URI ||
-  "mongodb://user1:password1@ds019976.mlab.com:19976/heroku_l9q6jn7c",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useMongoClient: true,
-  }
-);
+mongoose
+  .connect(
+    process.env.MONGODB_URI ||
+      "mongodb://user1:password1@ds019976.mlab.com:19976/heroku_l9q6jn7c",
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      // useMongoClient: true,
+    }
+  )
+  .then(() => console.log("DB Connected successfully!"))
+  .catch((err) => {
+    console.log("DB connection error: " + err);
+  });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on http://localhost:${PORT}!`);
 });
